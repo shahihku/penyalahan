47c3d570bdabdbfa46ada00ed901dfdb45932033
shahihku <121048524+shahihku@users.noreply.github.com>
2022-12-28 03:05:12 +0700

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-apt update
+#update
+apt update -y
+apt install -y python3 python3-dnslib net-tools
+apt install ncurses-utils -y
+apt install dnsutils -y
+apt install golang -y
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install ncurses-utils -y
+apt install screen -y
+apt install cron -y
+apt install iptables -y
+apt install -y git screen whois dropbear wget
+apt install -y pwgen python php jq curl
+apt install -y sudo gnutls-bin
+apt install -y mlocate dh-make libaudit-dev build-essential
+apt install -y dos2unix debconf-utils
+service cron reload
+apt install python ruby -y
+gem install lolcat
+service cron restart
 apt install sshpass -y
 version='1.0'
 ipip=$(curl ipinfo.io/ip)
